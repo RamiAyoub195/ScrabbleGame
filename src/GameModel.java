83078cfdd281dff3acc3a8fa2d0f8d8e88da4143
ramia <test@test.com>
2024-10-18 20:01:35 -0400

    Added the Txt.file with the words

--- src/GameModel.java
+++ src/GameModel.java
@@ -11,35 +11,79 @@ public class GameModel {
     private Player player;
     private Board board;
     private TilesBag tilesBag;
+    private WordList wordList;
 
 
     public GameModel(){
         player = new Player("Rami");
         board = new Board(15, 15);
         tilesBag = new TilesBag();
+        wordList = new WordList();
         playGame();
     }
 
     public void playGame(){
 
-        Random rand = new Random();
-        for (int i = 0; i < 7; i++){
-            int rnd = rand.nextInt(tilesBag.bagArraylist().size());
-            player.getTiles().add(tilesBag.bagArraylist().get(rnd));
-            tilesBag.bagArraylist().remove(rnd);
-        }
-
-        for (int i = 0; i < tilesBag.bagArraylist().size(); i++){
-            System.out.println(tilesBag.bagArraylist().get(i).toString());
-        }
-
+        getRandomTile(7, player);
         board.displayBoard();
         player.displayPlayer();
 
+        while (!checkGameFinished()){
+            playerTurn(player);
+        }
+    }
+
+    public void playerTurn(Player player){
+        System.out.println("Here are your options: Place Tile(s) (0), Swap Tiles (1), Skip Turn (2)");
+        Scanner userInput = new Scanner(System.in);
+
+        int choice = userInput.nextInt();
+
+        if (choice == 0){
+            System.out.println("Which tile would you like to place? (Enter the index (0-6)): ");
+            int choiceTile = userInput.nextInt();
+            Tiles t = player.getTiles().get(choiceTile);
+            player.getTiles().remove(choiceTile);
+            getRandomTile( 1, player);
+
+
+            System.out.println("Which row would you like to place the tile: ");
+            int row = userInput.nextInt();
+            System.out.println("Which column would you like to place the tile: ");
+            int column = userInput.nextInt();
+            board.placeBoardTile(row, column, t.getLetter());
+            board.displayBoard();
+        }
+        else if (choice == 1){
+            System.out.println("Which tile would you like to swap? (Enter the index (0-6)): ");
+            int choiceTile = userInput.nextInt();
+            Tiles t = player.getTiles().get(choiceTile);
+            player.getTiles().remove(choiceTile);
+            getRandomTile( 1, player);
+            tilesBag.bagArraylist().add(t);
+        }
+        else if (choice == 2){
+            //do nothing
+        }
     }
 
     public boolean checkGameFinished(){
         return tilesBag.bagArraylist().isEmpty();
+    }
+
+    /**
+     * Gets a random tile from the bag of tiles to a player
+     *
+     * @param numOfTiles number of tiles needed from bag
+     * @param player player who needs the tiles
+     */
+    public void getRandomTile(int numOfTiles, Player player){
+        Random rand = new Random();
+        for (int i = 0; i < numOfTiles; i++){
+            int rnd = rand.nextInt(tilesBag.bagArraylist().size());
+            player.getTiles().add(tilesBag.bagArraylist().get(rnd));
+            tilesBag.bagArraylist().remove(rnd);
+        }
     }
 
 
---
+++ src/WordList.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * This class will contain a list of all valid words. when a player tries to place a word(s) on the board.
+ * It will be checked by the list of valid words in this class. It opens a text file that contains a list
+ * of words from the example in the milestone project document on brightspace "https://www.mit.edu/~ecprice/wordlist.10000"
+ *
+ */
+
+public class WordList {
+
+    private ArrayList<String> words; //ArrayList of the words from the file
+
+    /**
+     * Initializes the ArrayList of words and opens and reads from the text file
+     * adding each word to the list of words.
+     */
+    public WordList() {
+
+        words = new ArrayList<>(); //Initializes the ArrayList
+
+        File filename = new File("wordlist.txt"); //Creates a file that the scanner will scan
+
+        try(Scanner scanner = new Scanner(filename)){ //Scanner will scan the file line by line
+            while(scanner.hasNextLine()){ //checks to see if there is a next line
+                words.add(scanner.nextLine().trim()); //add the word and gets rid of extra spaces/characters
+            }
+        }
+        catch(FileNotFoundException e){ //If the file cannot be found in the directory raise exception
+            System.out.println(e);
+        }
+
+    }
+
+    /**
+     * Returns the list of words that were obtained from the text file.
+     * @return ArrayList of words
+     */
+    public ArrayList<String> getWords() {
+        return words;
+    }
+
+    /**
+     * Returns true or false if the given word is found within the list of words.
+     * @param word the word being checked
+     * @return true if the word is found and false if the word is not found
+     */
+    public boolean isValidWord(String word){
+        return words.contains(word);
+    }
+
+}
