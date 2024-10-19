8d892540f6b5c3b84d32081b8b19bb6d4b83fae3
ramia <test@test.com>
2024-10-19 12:14:19 -0400

    woking on continuing the logic

--- src/GameModel.java
+++ src/GameModel.java
@@ -7,17 +7,21 @@ import java.util.*;
  */
 
 public class GameModel {
-
-    private ArrayList<Player> player;
-    private Board board;
+    private Scanner userInput;
+    private ArrayList<Player> players;
+    private Board gameBoard;
+    private Board checkBoard;
     private TilesBag tilesBag;
     private WordList wordList;
     private int numOfPlayers;
 
 
+
     public GameModel(){
-        player = new ArrayList<>();
-        board = new Board(15, 15);
+        userInput = new Scanner(System.in);
+        players = new ArrayList<>();
+        gameBoard = new Board(15, 15);
+        checkBoard = new Board(15, 15);
         tilesBag = new TilesBag();
         wordList = new WordList();
         numOfPlayers = 0;
@@ -26,7 +30,6 @@ public class GameModel {
 
     public void playGame(){
 
-        Scanner userInput = new Scanner(System.in);
         System.out.println("Welcome to the game of Scrabble! Please enter the number of players (2-4):");
         int numOfPlayers = userInput.nextInt();
 
@@ -38,13 +41,13 @@ public class GameModel {
         for(int i = 1; i <= numOfPlayers; i++){
             System.out.println("Please enter player " + i + " name:");
             String playerName = userInput.next();
-            player.add(new Player(playerName));
-            getRandomTiles(7, player.get(i - 1));
+            players.add(new Player(playerName));
+            getRandomTiles(7, players.get(i - 1));
         }
 
         while (!checkGameFinished()){
-            for(Player player : player){
-                board.displayBoard();
+            for(Player player : players){
+                gameBoard.displayBoard();
                 player.displayPlayer();
                 playerTurn(player);
             }
@@ -72,37 +75,34 @@ public class GameModel {
     }
 
     public void playerPlaceTile(Player player){
+        ArrayList<Tiles> tempTiles = new ArrayList<>();
+        ArrayList<Integer> tempRowPositions = new ArrayList<>();
+        ArrayList<Integer> tempColPositions = new ArrayList<>();
+
         Scanner userInput = new Scanner(System.in);
         System.out.println("How many tile(s) would you like to place: ");
         int choiceNumTiles = userInput.nextInt();
 
-        while(choiceNumTiles > 0){
-            System.out.println("Select a tile to place (Enter the index (0-6)): ");
+        while(choiceNumTiles > player.getTiles().size()) {
+            System.out.println("Invalid, user does not have enough tiles! Please enter the number of tile(s) you would like to place: ");
+            choiceNumTiles = userInput.nextInt();
+        }
+
+        System.out.println("You will now be entering the word to place tile by tile make sure to enter it in the right order!");
+
+        for (int i = 1; i <= choiceNumTiles; i++){
+            System.out.println("Please enter tile " + i + " (index 0-6):");
             int tileIndex = userInput.nextInt();
-
-            Tiles t = player.getTiles().get(tileIndex);
-            player.getTiles().remove(tileIndex);
-            getRandomTiles( 1, player);
-
-            System.out.println("Which row would you like to place the tile: ");
-            int row = userInput.nextInt();
-            System.out.println("Which column would you like to place the tile: ");
-            int column = userInput.nextInt();
-
-            if(board.checkBoardTileEmpty(row, column)){
-                if(board.checkLegalPlacement(row, column)){
-                    board.placeBoardTile(row, column, t.getLetter());
-                    board.displayBoard();
-                    player.displayPlayer();
-                    choiceNumTiles--;
-                }
-                else {
-                    System.out.println("Tile must be connected horizontally or vertically");
-                }
-            }
-            else{
-                System.out.println("That tile is already placed");
-            }
+            tempTiles.add(player.getTiles().get(tileIndex));
+            System.out.println("Please enter the row position for tile " + i + " :");
+            int rowIndex = userInput.nextInt();
+            tempRowPositions.add(rowIndex);
+            System.out.println("Please enter the column position for tile " + i + " :");
+            int columnIndex = userInput.nextInt();
+            tempColPositions.add(columnIndex);
+            System.out.println("Which direction is the word being placed (Horizontal/Vertical):");
+            String direction = userInput.next();
+            checkPlacement(tempTiles, tempRowPositions, tempColPositions, direction);
         }
     }
 
@@ -120,7 +120,7 @@ public class GameModel {
 
             getRandomTiles( 1, player);
             tilesBag.bagArraylist().add(t);
-            board.displayBoard();
+            gameBoard.displayBoard();
             player.displayPlayer();
             choiceNumTiles--;
         }
@@ -146,6 +146,54 @@ public class GameModel {
             int rnd = rand.nextInt(tilesBag.bagArraylist().size());
             player.getTiles().add(tilesBag.bagArraylist().get(rnd));
             tilesBag.bagArraylist().remove(rnd);
+        }
+    }
+
+    public boolean checkPlacement(ArrayList<Tiles> tiles, ArrayList<Integer> rows, ArrayList<Integer> cols, String direction){
+        if(gameBoard.checkMiddleBoardEmpty()){
+            checkBoard.placeBoardTile(rows.get(0), cols.get(0), tiles.get(0).getLetter());
+            for(int i = 1; i < tiles.size() - 1; i++){
+                if(checkBoard.checkIllegalPlacement(rows.get(i),cols.get(i))){
+                    System.out.println("Words must be connected horizontally or vertically! and the first word must cover over the center of the board");
+                    return false;
+                }
+                checkBoard.placeBoardTile(rows.get(i), cols.get(i), tiles.get(i).getLetter());
+                checkWord(checkBoard, rows.get(i), cols.get(i), direction, false);
+            }
+            checkWord(checkBoard,rows.get(rows.size() - 1), cols.get(cols.size() - 1), direction, true);
+        }
+        else if (board.checkBoardTileEmpty(row, column) && !board.checkIllegalPlacement(row, column)){
+            board.placeBoardTile(row, column, t.getLetter());
+            board.displayBoard();
+            player.displayPlayer();
+            player.getTiles().remove(tileIndex);
+            getRandomTiles( 1, player);
+            choiceNumTiles--;
+        }
+    }
+
+    public boolean checkWord(Board board, int row, int col, String direction, boolean lastLetter){
+        String wordLeftToRight = "";
+        String wordRightToLeft = "";
+        StringBuilder wordTopToBottom = new StringBuilder();
+        StringBuilder wordBottomToTop = new StringBuilder();
+
+        if (direction.equals("Horizontal") && !lastLetter){
+            int i = 1;
+            while(!board.getBoard()[row + i][col].equals("  -  ") && row + i < 15){
+                wordTopToBottom.append(board.getBoard()[row + i][col]);
+                i++;
+            }
+            int j = 1;
+            while(!board.getBoard()[row - j][col].equals("  -  ") && row - j >= 0){
+                wordBottomToTop.append(board.getBoard()[row - j][col]);
+                j++;
+            }
+            wordTopToBottom.reverse().append(wordBottomToTop);
+
+        }
+        else if (direction.equals("Vertical") && !lastLetter){
+
         }
     }
 
